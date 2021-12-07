6d0f59374aef328d7463efcc4ea8dfb134e004d5
Chaika9 <bestdevelop22@gmail.com>
2021-12-07 15:21:55 +0100

    :recycle: Minor refactor

--- src/main/java/fr/modded/api/model/admin/AdminPlayerServerGeneralInfoModel.java
+++ src/main/java/fr/modded/api/model/admin/AdminPlayerServerGeneralInfoModel.java
@@ -9,10 +9,14 @@ import java.util.UUID;
 public class AdminPlayerServerGeneralInfoModel {
     private final ClientServer server;
     private final List<UUID> whitelist;
+    private final String host;
+    private final int port;
 
-    public AdminPlayerServerGeneralInfoModel(ClientServer server, List<UUID> whitelist) {
+    public AdminPlayerServerGeneralInfoModel(ClientServer server, List<UUID> whitelist, String host, int port) {
         this.server = server;
         this.whitelist = whitelist;
+        this.host = host;
+        this.port = port;
     }
 
     public IPlayerServer getServer() {
@@ -21,5 +25,13 @@ public class AdminPlayerServerGeneralInfoModel {
 
     public List<UUID> getWhitelist() {
         return whitelist;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
     }
 }
